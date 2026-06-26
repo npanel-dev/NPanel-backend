bc15bb130f17c25c603126a137f63dd0e1389c72
NPanel Development Team <noreply@npanel.dev>
2026-06-26 23:42:29 +0700

    fix: include price options in user subscriptions

--- api/public/user/v1/user.pb.validate.go
+++ api/public/user/v1/user.pb.validate.go
@@ -2341,6 +2341,40 @@ func (m *Subscribe) validate(all bool) error {
 
 	// no validation rules for UpdatedAt
 
+	for idx, item := range m.GetPriceOptions() {
+		_, _ = idx, item
+
+		if all {
+			switch v := interface{}(item).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, SubscribeValidationError{
+						field:  fmt.Sprintf("PriceOptions[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, SubscribeValidationError{
+						field:  fmt.Sprintf("PriceOptions[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return SubscribeValidationError{
+					field:  fmt.Sprintf("PriceOptions[%v]", idx),
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
 	if len(errors) > 0 {
 		return SubscribeMultiError(errors)
 	}
@@ -2417,6 +2451,136 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = SubscribeValidationError{}
+
+// Validate checks the field values on SubscribePriceOption with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the first error encountered is returned, or nil if there are no violations.
+func (m *SubscribePriceOption) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on SubscribePriceOption with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the result is a list of violation errors wrapped in
+// SubscribePriceOptionMultiError, or nil if none found.
+func (m *SubscribePriceOption) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *SubscribePriceOption) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for Id
+
+	// no validation rules for SubscribeId
+
+	// no validation rules for Name
+
+	// no validation rules for DurationUnit
+
+	// no validation rules for DurationValue
+
+	// no validation rules for Price
+
+	// no validation rules for OriginalPrice
+
+	// no validation rules for Inventory
+
+	// no validation rules for Show
+
+	// no validation rules for Sell
+
+	// no validation rules for IsDefault
+
+	// no validation rules for Sort
+
+	// no validation rules for CreatedAt
+
+	// no validation rules for UpdatedAt
+
+	if len(errors) > 0 {
+		return SubscribePriceOptionMultiError(errors)
+	}
+
+	return nil
+}
+
+// SubscribePriceOptionMultiError is an error wrapping multiple validation
+// errors returned by SubscribePriceOption.ValidateAll() if the designated
+// constraints aren't met.
+type SubscribePriceOptionMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m SubscribePriceOptionMultiError) Error() string {
+	msgs := make([]string, 0, len(m))
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m SubscribePriceOptionMultiError) AllErrors() []error { return m }
+
+// SubscribePriceOptionValidationError is the validation error returned by
+// SubscribePriceOption.Validate if the designated constraints aren't met.
+type SubscribePriceOptionValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e SubscribePriceOptionValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e SubscribePriceOptionValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e SubscribePriceOptionValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e SubscribePriceOptionValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e SubscribePriceOptionValidationError) ErrorName() string {
+	return "SubscribePriceOptionValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e SubscribePriceOptionValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sSubscribePriceOption.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = SubscribePriceOptionValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = SubscribePriceOptionValidationError{}
 
 // Validate checks the field values on SubscribeDiscount with the rules defined
 // in the proto definition for this message. If any rules are violated, the
